72f070e53a3f6bf891f96202bb6b5af7e8797d18
Meidencore <danieleverdale@gmail.com>
2022-10-17 20:55:16 -0400

    debug background

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ var color2 = document.querySelector(".color2");
 // function declaration
 
 function setGradient() {
-    body.style.background = 
+    body.style.backgroundImage = 
     "linear-gradient(to right, "
     + color1.value
     + ", "
     + color2.value
     + ")";
 
-    css.textContent = body.style.background + ";"; 
+    css.textContent = body.style.backgroundImage + ";"; 
 }
 
 // add event listener
